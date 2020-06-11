1b9d2cceff15dc6f99c4298078304faf5fe9ea94
Tillmann Weimer <tillmann1970@gmail.com>
2020-06-11 18:31:17 +0200

    added delete button / form method

--- resources/views/shoppinglists/show.blade.php
+++ resources/views/shoppinglists/show.blade.php
@@ -6,17 +6,24 @@
     <div class="jumbotron col -md-12">
         <h1 class="text-center">{{ $shoppinglist->title }}</h1>
         <div class="text-center">
-            <ol>
-                @foreach($shoppinglist->shoppingitems as $shoppingitem)
-                    <br>
-                        {{ __('Article: ') }}{{ $shoppingitem->name }}<br>
-                        {{ __('Category: ') }}{{ $shoppingitem->category }}<br>
-                        {{ __('Quantity: ') }}{{ $shoppingitem->quantity }}<br>
-                        <small><i>{{ __('created by: ') }}{{ Auth::user()->name }}</i></small><br>
-                        <small><i>{{ __('at: ') }}{{ $shoppingitem->updated_at }}</i></small><br>
-                    </li>
-                @endforeach
-            </ol><hr>
+            <form method="post" action="/shoppingitems/delete">
+                @csrf
+                <ol>
+                    @foreach($shoppinglist->shoppingitems as $shoppingitem)
+                        <br>
+
+                        <input type="checkbox" name="ids[]" value="{{ $shoppingitem->id }}">&nbsp;
+                            {{ __('Article: ') }}{{ $shoppingitem->name }}<br>
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ __('Category: ') }}{{ $shoppingitem->category }}<br>
+                            &nbsp;&nbsp;&nbsp;{{ __('Quantity: ') }}{{ $shoppingitem->quantity }}<br>
+                            <small><i>{{ __('created by: ') }}{{ Auth::user()->name }}</i></small><br>
+                            <small><i>{{ __('at: ') }}{{ $shoppingitem->updated_at }}</i></small><br><br>
+
+
+                    @endforeach
+                </ol><hr>
+                <input type="submit" class="btn btn-danger" value="{{ __('Delete selected Item') }}"><br>
+            </form>
         </div>
     </div>
 </div>
