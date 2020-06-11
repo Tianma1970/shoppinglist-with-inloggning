f78c72e9f0dcc821a5d915920baac26634709a21
Tillmann Weimer <tillmann1970@gmail.com>
2020-06-11 18:04:11 +0200

    created index method

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Shoppinglist;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,10 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $shoppinglists = Shoppinglist::orderBy('title')->get();
+
+        return view('home', [
+            'shoppinglists'     => $shoppinglists
+        ]);
     }
 }
