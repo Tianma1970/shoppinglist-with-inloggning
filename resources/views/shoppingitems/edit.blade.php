33cd5928b8aa1ddf579dcdf1b09869848d537f6a
Tillmann Weimer <tillmann1970@gmail.com>
2020-06-11 19:36:35 +0200

    created fiorm for editing Shoppingitems

---
+++ resources/views/shoppingitems/edit.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts/app')
+
+@section('content')
+
+<div class="container">
+    @include('partials/status')
+    <div class="jumbotron col-12">
+
+        <h1 class="text-center">{{ __('Edit ') }}{{ $shoppingitem->name }}</h1>
+
+        <form method="POST" action="/shoppingitem/{{$shoppingitem->id}}/update">
+            <!--We need to set a csrf-token (Cross site request forgery)in order to send the form-->
+            @csrf
+            <!--/We need to set a csrf-token in order to send the form-->
+            @method('PUT')
+            <div class="form-group row">
+                <label for="category" class="col-md-4 text-md-right control-label">{{ __('Category') }}</label>
+                <div class="col-md-6">
+                    <select class="form-control" name="category" id="category">
+                        <option value="">{{ __('Select a Category') }}</option>
+                        <option value="food">{{ __('Food') }}</option>
+                        <option value="snack">{{ __('Snack') }}</option>
+                        <option value="other">{{ __('Other') }}</option>
+                    </select>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="name" class="col-md-4 text-md-right control-label">{{ __('Name') }}</label>
+                <div class="col-md-6">
+                    <input type="textarea" name ="name" id="name" class="form-control" required value="{{ old('name') ? old('name') : $shoppingitem->name }}">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="quantity" class="col-md-4 text-md-right control-label">{{ __('Quantity') }}</label>
+                <div class="col-md-6">
+                    <input type="textarea" name ="quantity" id="quantity" class="form-control" required value="{{ old('quantity') ? old('quantity') : $shoppingitem->quantity }}">
+                </div>
+            </div>
+            <input type="submit" class="btn btn-info" value="Edit Shopping item">
+            <a href="/shoppingitems/create" class="btn btn-info">{{ __('Back') }}</a>
+        </form>
+    </div>
+
+
+    @include('/templates/footer')
+</div>
+
+
+
+@endsection
