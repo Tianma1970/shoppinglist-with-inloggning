4db650eefd796c797829f80e7ecd22355c5cba1d
Tillmann Weimer <tillmann1970@gmail.com>
2020-06-11 12:55:49 +0200

    created file for creating and showing shoppinglists

---
+++ resources/views/shoppinglists/create.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts/app')
+
+@section('content')
+<div class="container">
+    @include('partials/status')
+    <div class="jumbotron col-md-12">
+        <h1 class="text-center">{{ __('Create a Shoppinglist') }}</h1>
+        <form method="post" action="/shoppinglist/store">
+            <!--We need to set a csrf token in order to send the form-->
+            @csrf
+            <div class="form-group row">
+                <label for="title" class="col-md-4 text-md-right control-label">{{ __('Title') }}</label>
+                <div class="col-md-6">
+                    <input type="textarea" name ="title" id="quantity" class="form-control" placeholder="The title of your Shoppinglist">
+                </div>
+                <input type="submit" class="btn btn-info" value="Save your Shopping list">
+            </div>
+        </form>
+    </div>
+
+    <div class="jumbotron col-md-12">
+        <h1>{{ __('Your Shoppinglists') }}</h1>
+        <ul>
+            @foreach($shoppinglists as $shoppinglist)
+            <li>
+                {{ $shoppinglist->title}}
+            </li>
+            @endforeach
+
+        </ul>
+    </div>
+    @include('/templates/footer')
+</div>
+@endsection
