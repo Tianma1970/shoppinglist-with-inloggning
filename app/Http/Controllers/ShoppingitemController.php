4d8e75b16e960ff58ee0efd08a79ae3374059aab
Tillmann Weimer <tillmann1970@gmail.com>
2020-06-11 11:58:34 +0200

    created entities Shoppinglist / Shoppingitem and migrationfiles

---
+++ app/Http/Controllers/ShoppingitemController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Shoppingitem;
+use Illuminate\Http\Request;
+
+class ShoppingitemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Shoppingitem  $shoppingitem
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Shoppingitem $shoppingitem)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Shoppingitem  $shoppingitem
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Shoppingitem $shoppingitem)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Shoppingitem  $shoppingitem
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Shoppingitem $shoppingitem)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Shoppingitem  $shoppingitem
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Shoppingitem $shoppingitem)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/ShoppinglistController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Shoppinglist;
+use Illuminate\Http\Request;
+
+class ShoppinglistController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Shoppinglist  $shoppinglist
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Shoppinglist $shoppinglist)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Shoppinglist  $shoppinglist
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Shoppinglist $shoppinglist)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Shoppinglist  $shoppinglist
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Shoppinglist $shoppinglist)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Shoppinglist  $shoppinglist
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Shoppinglist $shoppinglist)
+    {
+        //
+    }
+}
