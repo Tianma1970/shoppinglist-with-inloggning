fd0a5ae4106ffeb326d51f1e933dbb2ad05cc4d9
Tillmann Weimer <tillmann1970@gmail.com>
2020-06-11 18:03:49 +0200

    created $fillable variable / relationships

--- app/Shoppingitem.php
+++ app/Shoppingitem.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Shoppingitem extends Model
 {
-    //
+    protected $fillable = [
+        'shoppinglist_id',
+        'name',
+        'category',
+        'quantity'
+    ];
+
+    public function shoppinglist()
+    {
+        return $this->belongsTo(Shoppinglist::class);
+    }
 }
--- app/Shoppinglist.php
+++ app/Shoppinglist.php
@@ -2,9 +2,28 @@
 
 namespace App;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Shoppinglist extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'title'
+    ];
+
+    public function shoppinglist()
+    {
+        return $this->belongsTo(Shoppinglist::class);
+    }
+
+    public function shoppingitems()
+    {
+        return $this->hasMany(Shoppingitem::class);
+    }
+
+    public function user()
+    {
+        return $this->hasMany(Shoppinglist::class);
+    }
 }
